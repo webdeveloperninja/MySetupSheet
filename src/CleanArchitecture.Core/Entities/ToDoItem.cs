77e9fbd564c111565dab3f95fc6eb35741c17d0e
Your Name <rsmith5901@gmail.com>
2018-12-29 14:09:56 -0500

    sdsdf

--- src/CleanArchitecture.Core/Entities/ToDoItem.cs
+++ src/CleanArchitecture.Core/Entities/ToDoItem.cs
@@ -3,16 +3,16 @@ using CleanArchitecture.Core.SharedKernel;
 
 namespace CleanArchitecture.Core.Entities
 {
-    public class ToDoItem : BaseEntity
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public bool IsDone { get; private set; }
+  public class ToDoItem : BaseEntity
+  {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool? IsDone { get; private set; }
 
-        public void MarkComplete()
-        {
-            IsDone = true;
-            Events.Add(new ToDoItemCompletedEvent(this));
-        }
+    public void MarkComplete()
+    {
+      IsDone = true;
+      Events.Add(new ToDoItemCompletedEvent(this));
     }
+  }
 }
--- src/CleanArchitecture.Web/ApiModels/ToDoItemDTO.cs
+++ src/CleanArchitecture.Web/ApiModels/ToDoItemDTO.cs
@@ -7,24 +7,24 @@ using CleanArchitecture.Core.Entities;
 
 namespace CleanArchitecture.Web.ApiModels
 {
-    // Note: doesn't expose events or behavior
-    public class ToDoItemDTO
-    {
-        public int Id { get; set; }
-        [Required]
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public bool IsDone { get; private set; }
+  // Note: doesn't expose events or behavior
+  public class ToDoItemDTO
+  {
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool IsDone { get; private set; }
 
-        public static ToDoItemDTO FromToDoItem(ToDoItem item)
-        {
-            return new ToDoItemDTO()
-            {
-                Id = item.Id,
-                Title = item.Title,
-                Description = item.Description,
-                IsDone = item.IsDone
-            };
-        }
+    public static ToDoItemDTO FromToDoItem(ToDoItem item)
+    {
+      return new ToDoItemDTO()
+      {
+        Id = item.Id,
+        Title = item.Title,
+        Description = item.Description,
+        IsDone = Convert.ToBoolean(item.IsDone)
+      };
     }
+  }
 }
