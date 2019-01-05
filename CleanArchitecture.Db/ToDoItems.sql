65b4b2e5b9ec8b49f888316545ad36a9591cfa5d
Your Name <rsmith5901@gmail.com>
2019-01-05 10:49:43 -0500

    sfdfsd

---
+++ CleanArchitecture.Db/ToDoItems.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ToDoItems]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] VARCHAR(50) NOT NULL, 
+    [Description] VARCHAR(MAX) NULL, 
+    [IsDone] BIT NOT NULL DEFAULT 0
+)
