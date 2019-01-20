adf6c3f3bdb5829ae4bb0e6a755659a4bb590fb6
Your Name <rsmith5901@gmail.com>
2019-01-20 14:29:17 -0500

    sdf

---
+++ Api/CleanArchitecture.Core/Queries/GetTodoByIdHandler.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Core.Queries
+{
+    public class GetTodoByIdRequest : IRequest<ToDoItem>
+    {
+        public int Id { get; set; }
+    }
+
+    public class GetTodoByIdHandler : IRequestHandler<GetTodoByIdRequest, ToDoItem>
+    {
+        private readonly IRepository _repository;
+
+        public GetTodoByIdHandler(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ToDoItem> Handle(GetTodoByIdRequest request, CancellationToken cancellationToken)
+        {
+            return  _repository.GetById<ToDoItem>(request.Id);
+        }
+    }
+}
