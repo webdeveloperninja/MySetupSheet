20890abfaaa7ba0b5384dd614c4c4e0d7ad9c1d7
Your Name <rsmith5901@gmail.com>
2019-02-02 18:19:40 -0500

    headlerss

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -25,7 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      MyHeadlessChrome: {
+        base: 'ChromeHeadless',
+        flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223']
+      }
+    },
     singleRun: false
   });
 };
