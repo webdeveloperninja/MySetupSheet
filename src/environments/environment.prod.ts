f3a60653f282fc53373b2132f3cf1492f7edb007
Your Name <rsmith5901@gmail.com>
2019-02-03 18:17:24 -0500

    sadfsfdasadf sfdasdaf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  instrumentationKey: JSON.stringify('$(instrumentationKey)$'),
-  production: !!'$(production)$'
+  production: true,
+  instrumentationKey: '6b4997f7-abf2-4dc7-815a-452ba722ff53'
 };
