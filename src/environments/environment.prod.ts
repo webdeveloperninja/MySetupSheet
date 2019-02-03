904b505fd63bb419cf02f2c0775fb0d250d3a9c6
Your Name <rsmith5901@gmail.com>
2019-02-03 15:56:33 -0500

    afsdfsd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  instrumentationKey: '#{ instrumentationKey }#',
-  production: !!'#{ production }#'
+  instrumentationKey: '#{instrumentationKey}#',
+  production: !!'#{production}#'
 };
