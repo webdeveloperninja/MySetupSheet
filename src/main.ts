e2d33324f40a5ddb8ccaf64f845ee18aae48d148
Your Name <rsmith5901@gmail.com>
2019-05-04 14:25:02 -0400

    asdfasdf fdsa dfasdfa s daf

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { environment } from 'src/enviroments/enviroment.dev';
 import { AppModule } from './app/app.module';
 
-declare let window: any;
+if (environment.production) {
+  enableProdMode();
+}
 
-const xmlHttp = new XMLHttpRequest();
-
-xmlHttp.onreadystatechange = function() {
-  if (this.readyState === 4 && this.status === 200) {
-    window.appSettings = JSON.parse(this.responseText);
-
-    if (window.appSettings.production) {
-      enableProdMode();
-    }
-
-    platformBrowserDynamic()
-      .bootstrapModule(AppModule)
-      .catch(err => console.error(err));
-  }
-};
-
-xmlHttp.open('GET', 'assets/data/settings.json', true);
-xmlHttp.send();
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
