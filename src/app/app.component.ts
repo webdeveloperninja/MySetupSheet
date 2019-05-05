e4b69d49eb37e535e9aed4dc25d55cc7d0abcf51
Your Name <rsmith5901@gmail.com>
2019-05-04 21:03:17 -0400

    asdffsdadsfada fsfda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subject } from 'rxjs';
+declare let ga: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent implements OnDestroy {
+  private onDestroy$ = new Subject();
+
+  constructor(private readonly router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+  }
+}
