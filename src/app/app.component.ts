6d3153c81a70656128e6393a8f0ffd270f6d4486
Your Name <rsmith5901@gmail.com>
2019-05-05 13:03:24 -0400

    sadf sdfasdf adsf a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { AppInsightService } from './core/services/app-insights.service';
 declare let ga: Function;
 
 @Component({
@@ -11,11 +12,13 @@ declare let ga: Function;
 export class AppComponent implements OnDestroy {
   private onDestroy$ = new Subject();
 
-  constructor(private readonly router: Router) {
+  constructor(private readonly router: Router, private readonly appInsights: AppInsightService) {
     this.router.events.pipe(takeUntil(this.onDestroy$)).subscribe(event => {
       if (event instanceof NavigationEnd) {
         ga('set', 'page', event.urlAfterRedirects);
         ga('send', 'pageview');
+
+        this.appInsights.logPageView('Root');
       }
     });
   }
