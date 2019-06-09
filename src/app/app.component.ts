2b20d87fcd652830433267035598505c4a413206
Your Name <rsmith5901@gmail.com>
2019-06-09 15:56:28 -0400

    asdfsdafdfsa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,11 @@ export class AppComponent implements OnDestroy {
 
   constructor(private readonly router: Router, private readonly appInsights: AppInsightService) {
     this.router.events.pipe(takeUntil(this.onDestroy$)).subscribe(event => {
-      if (event instanceof NavigationEnd && !!ga) {
-        ga('set', 'page', event.urlAfterRedirects);
-        ga('send', 'pageview');
+      if (event instanceof NavigationEnd) {
+        if (!!ga) {
+          ga('set', 'page', event.urlAfterRedirects);
+          ga('send', 'pageview');
+        }
 
         this.appInsights.logPageView('Root');
       }
