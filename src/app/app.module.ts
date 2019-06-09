f615e93407aa3b08e853b13d5f8720ba656e8e0a
Your Name <rsmith5901@gmail.com>
2019-06-09 12:57:30 -0400

    sfdasfda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule, ErrorHandler } from '@angular/core';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { ClipboardModule } from 'ngx-clipboard';
+import { ImageCropperModule } from 'ngx-image-cropper';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SetupSheetComponent } from './containers/setup-sheet/setup-sheet.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ThemeModule } from './theme.module';
-import { ToolsComponent } from './components/tools/tools.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { ClipboardModule } from 'ngx-clipboard';
-import { GlobalErrorHandler } from './core/global-error-handler';
 import { DetailsComponent } from './components/details/details.component';
-import { TopBarComponent } from './components/top-bar/top-bar.component';
-import { ReportComponent } from './components/report/report.component';
 import { EmailReportComponent } from './components/email-report/email-report.component';
-import { PhotosComponent } from './components/photos/photos.component';
-import { ImageCropperModule } from 'ngx-image-cropper';
 import { PhotoCropperComponent } from './components/photo-cropper/photo-cropper.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from 'src/enviroments/enviroment.dev';
+import { PhotosComponent } from './components/photos/photos.component';
+import { ReportComponent } from './components/report/report.component';
+import { ToolsComponent } from './components/tools/tools.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { SetupSheetComponent } from './containers/setup-sheet/setup-sheet.component';
+import { GlobalErrorHandler } from './core/global-error-handler';
+import { ThemeModule } from './theme.module';
 
 @NgModule({
   declarations: [
@@ -46,7 +45,7 @@ import { environment } from 'src/enviroments/enviroment.dev';
     CommonModule,
     ClipboardModule,
     ImageCropperModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: true })
   ],
   providers: [
     {
