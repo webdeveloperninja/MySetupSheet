4875fcd08a44d536f430506e2256c85b757c54fd
Your Name <rsmith5901@gmail.com>
2019-06-29 14:14:26 -0400

    asdffsad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { AppInsightService } from './core/services/app-insights.service';
-declare let ga: Function;
 
 @Component({
   selector: 'app-root',
@@ -11,19 +7,6 @@ declare let ga: Function;
 })
 export class AppComponent implements OnDestroy {
   private onDestroy$ = new Subject();
-
-  constructor(private readonly router: Router, private readonly appInsights: AppInsightService) {
-    this.router.events.pipe(takeUntil(this.onDestroy$)).subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        if (!!ga) {
-          ga('set', 'page', event.urlAfterRedirects);
-          ga('send', 'pageview');
-        }
-
-        this.appInsights.logPageView('Root');
-      }
-    });
-  }
 
   ngOnDestroy() {
     this.onDestroy$.next();
